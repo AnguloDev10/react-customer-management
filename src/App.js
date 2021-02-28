03c65d8ffbe6669546935ca233a9e628c8f2cbf2
William Angulo <wmavalle@gmail.com>
2021-02-28 13:19:26 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import Table from './Table'
+import Form from './Form'
+class App extends Component {
+  state = {
+    characters : [
+      {
+        name: 'William',
+        job: 'Engineer',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
+  }
+  removeCharacter = (index) => {
+    const {characters} = this.state
+  
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+  handleSubmit = (character) => {
+    this.setState({characters: [...this.state.characters, character]})
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  render() {
+   const {characters} = this.state
+   return (
+    <div className="container">
+       <h1>Client Management</h1>
+       <p>Add a character with a name and a job to the table.</p>
+       <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+       />
+        <h3>Add New</h3>
+        <Form handleSubmit={this.handleSubmit} />
     </div>
-  );
+  )
+  }
 }
+
 
 export default App;
